43536577117658d361d79fea730bd2d12a1c27de
Yannis Van Achter <yannis.van.achter@gmail.com>
2023-03-19 19:06:27 +0100

    train

---
+++ app.js
@@ -0,0 +1,25 @@
+var nb_beers = 0;
+function main() {
+    var html = document.getElementsByTagName("body");
+
+    if (nb_beers == 0) {
+        // green #adff2f
+        
+    }
+    else if (nb_beers >= 3) {
+        // yellow #ffe72f
+    }
+    else if (nb_beers >= 5) {
+        // orange #ffa52f
+    }
+    else if (nb_beers >= 7) {
+        // red #ff2f2f
+    }
+}
+
+main();
+
+document.getElementsById("container-button").addEventListener("click", function(e) {
+    nb_beers++;
+    console.log(nb_beers);
+})
